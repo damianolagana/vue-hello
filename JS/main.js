4dd5b602494cd40b27ee58e24cb7a9dc4372f5dc
damianolagana <damianolagana@gmail.com>
2023-05-08 15:07:18 +0200

    add image

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,8 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        message: 'Benvenuto Utente'
+        message: 'Benvenuto Utente',
+        source: 'https://unsplash.it/300/300?image'
       }
     }
 }).mount('#app')
